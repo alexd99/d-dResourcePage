bfc29926f880eaeb533f321d15fae6f3c59a3e42
alexdahl <alexd@inwhatlanguage.com>
2018-08-10 10:27:29 -0600

    finished the classes page

--- js/classes.js
+++ js/classes.js
@@ -7,57 +7,143 @@ dndGetRequest('classes/', false)
   dndClasses.results.forEach((dndClass, index)=>{
     let classDiv = document.createElement('div');
     classDiv.classList.add('classDiv');
-    classDiv.innerHTML = `<h3>${dndClass.name}</h3><img src="./assets/images/dndClasses/${dndClass.name}.png" class="dndClassImage"><p>${classDescriptions[index].description}</p>`
+    classDiv.innerHTML = `<h3>${dndClass.name}</h3><img src="./assets/images/dndClasses/${dndClass.name}.png" class="dndClassImage"><p>${classDescriptions[index].description}</p>`;
     classDiv.addEventListener('click', showClass(dndClass.url));
     document.getElementById('dndClassesBox').appendChild(classDiv);
   })
 });
 
 function showClass(url) {
-  return ()=>{
+  return () => {
     dndGetRequest(url, true)
     .then((dndClass)=>{
-      console.log(dndClass);
-      let dndClassBox = document.getElementById('dndClassBox')
-      document.getElementById('dndClassesBox').style.display = 'none';
-      dndClassBox.style.display = 'block';
+      dndGetRequest(dndClass.starting_equipment.url, true)
+      .then((equipment) => {
 
-      let spellCasting = '';
-      if(dndClass.spellcasting){
-        spellCasting = `Spell Casting: ${dndClass.spellcasting.class}`
-      }
+        let dndClassBox = document.getElementById('dndClassBox');
+        document.getElementById('dndClassesBox').style.display = 'none';
+        dndClassBox.style.display = 'block';
 
-      let proficiencies = '';
-      dndClass.proficiencies.forEach((proficiency, index)=>{
-        if(dndClass.proficiencies.length - 1 === index){
-          proficiencies += `${proficiency.name}`
+        let spellCasting = '';
+        if(dndClass.spellcasting){
+          spellCasting = `<b>Spell Casting:</b> ${dndClass.spellcasting.class}`;
         }
-        else {
-          proficiencies += `${proficiency.name}, `
-        }
+
+        let proficiencies = '';
+        dndClass.proficiencies.forEach((proficiency, index)=>{
+          if(dndClass.proficiencies.length - 1 === index){
+            proficiencies += `${proficiency.name}`;
+          }
+          else {
+            proficiencies += `${proficiency.name}, `;
+          }
+        });
+
+        let proficiencyChoices = '';
+        dndClass.proficiency_choices.forEach((skill, index)=>{
+          proficiencyChoices += `<b>Choose ${skill.choose} Skills From:</b> `
+          skill.from.forEach((choice, i) => {
+            let choiceName = choice.name
+            if(choiceName.includes("Skill:")){
+              choiceName = choiceName.substr(7);
+            }
+
+           if(skill.from.length - 1 === i && dndClass.proficiency_choices.length - 1 !== index){
+              proficiencyChoices += `${choiceName} <br> <br>`;
+            }
+            else if(skill.from.length - 1 === i){
+              proficiencyChoices += `${choiceName}`;
+            }
+            else {
+              proficiencyChoices += `${choiceName}, `;
+            }
+          });
+        });
+
+        let savingThrows = ''
+        dndClass.saving_throws.forEach((save, index) => {
+          let savingThrow = save.name.toLowerCase();
+          savingThrow = savingThrow.charAt(0).toUpperCase() + savingThrow.slice(1);
+          if(dndClass.saving_throws.length - 1 === index){
+              savingThrows += `${savingThrow}`;
+          }
+          else {
+              savingThrows += `${savingThrow}, `;
+          }
+        })
+
+        let startingEquipment = ''
+        equipment.starting_equipment.forEach((item, index) => {
+          let name = item.item.name;
+          if(item.quantity > 1){
+            name += 's';
+          }
+          if(equipment.starting_equipment.length - 1 === index){
+              startingEquipment += `${item.quantity} ${name}`;
+          }
+          else {
+              startingEquipment += `${item.quantity} ${name}, `;
+          }
+        })
+
+        dndClassBox.innerHTML = `
+          <div id="classCard">
+            <div class="dndClassTitleBox">
+              <div class="dndClassTitleBoxTop">
+                <div></div>
+                <div id="classTitleBoxText">
+                  <h2 class="className">${dndClass.name}</h2>
+                </div>
+                <div><i class="fas fa-times closeClassBox" onclick="document.getElementById('dndClassesBox').style.display = 'flex';document.getElementById('dndClassBox').style.display = 'none';"></i></div>
+              </div>
+            </div>
+            <div class="classCardBody">
+              <div class="classCardBodyLeft">
+                <p><b>Proficiencies:</b> ${proficiencies}</p>
+                <p>${proficiencyChoices}</p>
+                <p><b>Starting Equipment:</b> ${startingEquipment}</p>
+              </div>
+              <div class="classCardBodyRight">
+                <p><b>Hit Die:</b> D${dndClass.hit_die}</p>
+                <p>${spellCasting}</p>
+                <p><b>Saving Throws:</b> ${savingThrows}</p>
+              </div>
+            </div>
+          </div>
+        `
+
+        let subclassEl = document.createElement('h3');
+        subclassEl.classList.add('subclassTitle');
+        subclassEl.addEventListener('click', showSubclass(dndClass.subclasses[0].url));
+        subclassEl.innerHTML = `Subclass: ${dndClass.subclasses[0].name}`;
+
+        document.getElementById('classTitleBoxText').appendChild(subclassEl);
       });
+    });
+  }
+}
 
-      dndClassBox.innerHTML = `
-        <div id="classCard">
-          <div class="dndClassTitleBox">
-            <div class="dndClassTitleBoxTop">
-              <div></div>
-              <div><h2 class="className">${dndClass.name}</h2></div>
-              <div><i class="fas fa-times" onclick="document.getElementById('dndClassesBox').style.display = 'flex';document.getElementById('dndClassBox').style.display = 'none';"></i></div>
-            </div>
-              <h3 class="subclassTitle">subclass: ${dndClass.subclasses[0].name}</h3>
-          </div>
-          <div class="classCardBody">
-            <div class="classCardBodyLeft">
-              <p><b>Proficiencies:</b> ${proficiencies}</p>
-            </div>
-            <div class="classCardBodyRight">
-              <p>Hit Die: D${dndClass.hit_die}</p>
-              <p>${spellCasting}</p>
-            </div>
-          </div>
-        </div>
-      `
-    })
+function showSubclass (url) {
+  return () => {
+    dndGetRequest(url, true)
+    .then((subclass) => {
+      const subclassDescription = new tingle.modal({
+          footer: true,
+          stickyFooter: false,
+          closeMethods: ['overlay', 'button', 'escape'],
+          closeLabel: "Close",
+          cssClass: ['modalTextAlignLeft'],
+          onOpen: function() {
+          },
+          onClose: function() {
+          },
+          beforeClose: function() {
+              return true; // close the modal
+          }
+      });
+      subclassDescription.setContent(`<p class="modalParagraph">${subclass.desc[0]}</p>`);
+      subclassDescription.open()
+
+    });
   }
 }
